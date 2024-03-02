274aa88218b5a4636690008ddacc62c61bb21ff8
ronnyscale <vladsolomennikov@mail.ru>
2024-03-02 20:50:55 +0700

    edit admin.py, add new urls, add static css, add templates and create list and detail.html

--- main/models.py
+++ main/models.py
@@ -6,22 +6,28 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
 
+    class PublishedManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, 
-                               on_delete=models.CASCADE,
-                               related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ["-publish"]
