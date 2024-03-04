ee6d0beabc7fe6d1552a0bf3c7c8a88a85015ff9
ronnyscale <vladsolomennikov@mail.ru>
2024-03-04 15:02:08 +0700

    add templatetags

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -43,9 +44,18 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма комментирования
     form = CommentForm()
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags', '-publish')[:4]
+
     return render(request, "post/detail.html", {"post": post,
                                                 "comments": comments,
-                                                "form": form})
+                                                "form": form,
+                                                "similar_posts": similar_posts})
 
 
 def post_share(request, post_id):
