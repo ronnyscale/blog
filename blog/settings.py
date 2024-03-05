8d489cb7e1ae28c8206f70dab02117d16f34c117
ronnyscale <vladsolomennikov@mail.ru>
2024-03-05 18:43:56 +0700

    add feed and dump database

--- blog/settings.py
+++ blog/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog', # BASE_DIR / 'db.sqlite3'
+        'USER': 'blog',
+        'PASSWORD': 'admin'
     }
 }
 
