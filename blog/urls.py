6b190c2cffe16569e730a32f28eeaa8d4176d1f1
ronnyscale <vladsolomennikov@mail.ru>
2024-03-05 17:11:26 +0700

    add sitemap and template filter

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from main.sitemaps import PostSitemap
+
+sitemaps = {
+    "posts": PostSitemap,
+}
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main')),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls", namespace="main")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
